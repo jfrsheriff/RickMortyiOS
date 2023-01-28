290c27589db5ab9be22f229e066f7d3b4d9912bf
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2023-01-28 14:33:02 +0530

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Jaffer Sheriff U on 28/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
