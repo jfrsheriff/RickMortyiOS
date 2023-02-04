d8180385e476dc7ffc26a012d305c8ec1974cd0a
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2023-02-04 21:27:45 +0530

    Request Manager Implementation

--- RickAndMorty/API Client/Request.swift
+++ RickAndMorty/API Client/Request.swift
@@ -8,5 +8,48 @@
 import Foundation
 
 final class Request{
+    private struct Constants{
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endPoint : EndPoint
+    private let pathComponents : [String]
+    private let queryParams : [URLQueryItem]
+    
+    public var urlString : String {
+        
+        var urlStr = Constants.baseUrl
+        urlStr += ("/" + endPoint.rawValue)
+        
+        if !pathComponents.isEmpty{
+            pathComponents.forEach {
+                urlStr += "/\($0)"
+            }
+        }
+        
+        if !queryParams.isEmpty{
+            urlStr += "?"
+            let queryStr = queryParams.compactMap {
+                "\($0.name)=\($0.value ?? "")"
+            }.joined(separator: "&")
+            urlStr += queryStr
+        }
+        
+        return urlStr
+    }
+    
+    public var url : URL? {
+        URL(string: urlString)
+    }
+    
+    public init(
+        endPoint : EndPoint,
+        pathComponents: [String] = [],
+        queryParams: [URLQueryItem] = []
+    ) {
+        self.endPoint = endPoint
+        self.pathComponents = pathComponents
+        self.queryParams = queryParams
+    }
     
 }
