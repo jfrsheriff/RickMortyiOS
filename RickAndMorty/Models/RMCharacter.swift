ea1d3e1042eb9919e5587b8421185874821a60ee
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2023-02-04 09:58:42 +0530

    Model Added

--- RickAndMorty/Models/RMCharacter.swift
+++ RickAndMorty/Models/RMCharacter.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct RMCharacter : Codable{
+    let id: Int
+    let name: String
+    let status: RMCharacterStatus
+    let species: String
+    let type: String
+    let gender: RMGender
+    let origin: RMOrgin
+    let location: RMSingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+enum RMCharacterStatus : String, Codable{
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
+
+enum RMGender : String, Codable{
+    case male = "Male"
+    case female = "Female"
+    case gender = "Genderless"
+    case `unknown` = "unknown"
+}
+
+struct RMOrgin : Codable{
+    let name : String
+    let url : String
+}
+
+struct RMSingleLocation : Codable{
+    let name : String
+    let url : String
+}
+
